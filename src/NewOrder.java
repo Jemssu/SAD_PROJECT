92a8445a9d1e2d07806fd63d3cf3ad0691f03fc1
James Nathaniel Tan <jemsofficialinbox@gmail.com>
2024-05-20 11:12:36 +0800

    Finalized the GUI things

--- src/NewOrder.java
+++ src/NewOrder.java
@@ -1,22 +1,112 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
+import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.DefaultTableModel;
 
 public class NewOrder extends JFrame {
     String bgColor = "#f7f1e3";
     @SuppressWarnings("unused")
     private Dashboard dashboard;
+    JPanel rightPanel, leftPanel;
+    JButton button1, button2, button3, button4, button5, button6;
+
+    /**
+     * This method is responsible for loading the image for the Icons in the Buttons Panel.
+     * @param path - source file path of the image
+     * @param width - width of the image (px)
+     * @param height - height of the image (px)
+     * @return
+     * @comments Note:
+     */
+    public static ImageIcon resizeIcon(String path, int width, int height) {
+        try {
+            ImageIcon icon = new ImageIcon(path);
+            Image img = icon.getImage();
+            Image newImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            return new ImageIcon(newImg);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * This class contains the ImageButton method. THis is responsible for 
+     * allowing the addition of padding to the image.
+     * @comments Note:
+     */
+    public static class ImageButton extends JButton {
+        private ImageIcon icon;
+        private int paddingTop; 
+        /**
+         * This method is responsible for adding padding to the image and
+         * lowers the text relative to the button.
+         * @param icon
+         * @param paddingTop
+         * @comments Note:
+         */
+        public ImageButton(ImageIcon icon, int paddingTop) {
+            this.icon = icon;
+            this.paddingTop = paddingTop;
+            setVerticalTextPosition(SwingConstants.BOTTOM);
+        }
+        /**
+         * This method allows irreversible changes. Making sure the components
+         * are located exactly where they are.
+         * @comments Note:
+         */
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (icon != null) {
+                int iconX = (getWidth() - icon.getIconWidth()) / 2;
+                int iconY = paddingTop; 
+                icon.paintIcon(this, g, iconX, iconY);
+            }
+        }
+    }
+
+    /**
+     * This method combined with ImageButton allows for the exact
+     * location of the TEXT inside the button. Such as changing the 
+     * margin / padding of it.
+     * @param button
+     * @param paddingTop
+     * @comments Note:
+     */
+    public static void adjustButtonTextHeight(AbstractButton button, int paddingTop) {
+        button.setVerticalTextPosition(SwingConstants.BOTTOM);
+        button.setMargin(new Insets(paddingTop, 0, 0, 0)); // Only Top since it is already centered.
+    }
 
     public NewOrder(String access_level, Dashboard dashboard) {
         this.dashboard = dashboard;
+
         JFrame newOrderFrame = new JFrame("New Order Panel");
         newOrderFrame.setSize(1680, 720);
         newOrderFrame.setLayout(null);
@@ -64,6 +154,237 @@ public class NewOrder extends JFrame {
 
         newOrderFrame.addWindowListener(windowAdapter);
         newOrderFrame.addWindowFocusListener(windowAdapter);
+
+        /**This is the left portion of the GUI. 
+         * @Comment Note:
+         */
+
+        leftPanel = new JPanel();
+        leftPanel.setBounds(5, 5, 830, 670);
+        leftPanel.setLayout(null);
+        //leftPanel.setBorder(BorderFactory.createTitledBorder("11"));
+        leftPanel.setBackground(Color.decode(bgColor));
+
+        newOrderFrame.add(leftPanel);
+
+        // Create buttons with icons aligned on top and padding
+        button1 = new ImageButton(resizeIcon("icons/no_new.png", 100, 100), 50);
+        button2 = new ImageButton(resizeIcon("icons/no_remove.png", 100, 100), 50);
+        button3 = new ImageButton(resizeIcon("icons/no_cancel.png", 100, 100), 50);
+        button4 = new ImageButton(resizeIcon("icons/no_check.png", 100, 100), 50);
+        button5 = new ImageButton(resizeIcon("icons/no_confirm.png", 100, 100), 50);
+        button6 = new ImageButton(resizeIcon("icons/back_exit.png", 100, 100), 50);
+
+        // Set Text of Buttons
+        button1.setText("New Order");
+        button2.setText("Remove Item");
+        button3.setText("Cancel Order");
+        button4.setText("Check Details");
+        button5.setText("Confirm Order");
+        button6.setText("Exit");
+
+        // Adjust text height for buttons
+        adjustButtonTextHeight(button1, 50);
+        adjustButtonTextHeight(button2, 50);
+        adjustButtonTextHeight(button3, 50);
+        adjustButtonTextHeight(button4, 50);
+        adjustButtonTextHeight(button5, 50);
+        adjustButtonTextHeight(button6, 50);
+
+        button1.setBackground(Color.decode("#ff793f"));
+        button2.setBackground(Color.decode("#ff793f"));
+        button3.setBackground(Color.decode("#ff793f"));
+        button4.setBackground(Color.decode("#ff793f"));
+        button5.setBackground(Color.decode("#ff793f"));
+        button6.setBackground(Color.decode("#ff793f"));
+
+        button1.setFocusPainted(false);
+        button2.setFocusPainted(false);
+        button3.setFocusPainted(false);
+        button4.setFocusPainted(false);
+        button5.setFocusPainted(false);
+        button6.setFocusPainted(false);
+
+        // Location of all the BUTTONS
+        button1.setBounds(20, 24, 250, 300);
+        button2.setBounds(290, 25, 250, 300);
+        button3.setBounds(560, 25, 250, 300);
+        button4.setBounds(20, 345, 250, 300);
+        button5.setBounds(290, 345, 250, 300);
+        button6.setBounds(560, 345, 250, 300);
+
+        // BUTTON 1 MODEL with LISTENER
+        button1.getModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                ButtonModel model = (ButtonModel) e.getSource();
+                if (model.isPressed()) {
+                    button1.setBackground(Color.decode("#33d9b2")); // Change color when pressed
+                    System.out.println("Button 1 - Is Pressed");
+                } else {
+                    button1.setBackground(Color.decode("#ff793f")); // Change color back when released
+                }
+            }
+        });
+        
+        // BUTTON 2 MODEL with LISTENER
+        button2.getModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                ButtonModel model = (ButtonModel) e.getSource();
+                if (model.isPressed()) {
+                    button2.setBackground(Color.decode("#33d9b2")); // Change color when pressed
+                    System.out.println("Button 2 - Is Pressed");
+                } else {
+                    button2.setBackground(Color.decode("#ff793f")); // Change color back when released
+                }
+            }
+        });
+
+        // BUTTON 3 MODEL with LISTENER
+        button3.getModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                ButtonModel model = (ButtonModel) e.getSource();
+                if (model.isPressed()) {
+                    button3.setBackground(Color.decode("#33d9b2")); // Change color when pressed
+                    System.out.println("Button 3 - Is Pressed");
+                } else {
+                    button3.setBackground(Color.decode("#ff793f")); // Change color back when released
+                }
+            }
+        });
+
+        // BUTTON 4 MODEL with LISTENER
+        button4.getModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                ButtonModel model = (ButtonModel) e.getSource();
+                if (model.isPressed()) {
+                    button4.setBackground(Color.decode("#33d9b2")); // Change color when pressed
+                    System.out.println("Button 4 - Is Pressed");
+                } else {
+                    button4.setBackground(Color.decode("#ff793f")); // Change color back when released
+                }
+            }
+        });
+
+        // BUTTON 5 MODEL with LISTENER
+        button5.getModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                ButtonModel model = (ButtonModel) e.getSource();
+                if (model.isPressed()) {
+                    button5.setBackground(Color.decode("#33d9b2")); // Change color when pressed
+                    System.out.println("Button 5 - Is Pressed");
+                } else {
+                    button5.setBackground(Color.decode("#ff793f")); // Change color back when released
+                }
+            }
+        });
+
+        // BUTTON 6 MODEL with CHANGE LISTENER (EXIT)
+        button6.getModel().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                ButtonModel model = (ButtonModel) e.getSource();
+                if (model.isPressed()) {
+                    button6.setBackground(Color.decode("#33d9b2")); // Change color when pressed
+                    System.out.println("Button 6 - Is Pressed");
+                } else {
+                    button6.setBackground(Color.decode("#ff793f")); // Change color back when released
+                }
+            }
+        });
+
+        // BUTTON 6 MODEL with ACTION LISTENER (EXIT)
+        button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int choice = JOptionPane.showConfirmDialog(newOrderFrame,
+                        "Are you sure you want to close? Any unsaved changes will be lost.",
+                        "Confirm Close", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+                System.out.println("Choice: " + choice);
+                if (choice == JOptionPane.YES_OPTION) {
+                    System.out.println("Closing the window.");
+                    newOrderFrame.dispose();
+                    dashboard.showMainDashboard();
+                } else {
+                    System.out.println("Window close canceled.");
+                }
+            }
+        });
+
+        // ADDING THE BUTTONS
+        leftPanel.add(button1);
+        leftPanel.add(button2);
+        leftPanel.add(button3);
+        leftPanel.add(button4);
+        leftPanel.add(button5);
+        leftPanel.add(button6);
+
+        /**This is the Right portion of the GUI. 
+         * @Comment Note:
+         */
+
+        rightPanel = new JPanel();
+        rightPanel.setBounds(830, 5, 830, 670);
+        rightPanel.setLayout(null);
+        //rightPanel.setBorder(BorderFactory.createTitledBorder("11"));
+        rightPanel.setBackground(Color.decode(bgColor));
+
+        newOrderFrame.add(rightPanel);
+
+        JLabel transaction_label = new JLabel("Transactions");
+        transaction_label.setBounds(5, 5, 250, 35);
+        rightPanel.add(transaction_label);
+
+        // Create a table model for the first table
+        DefaultTableModel transaction_TableModel = new DefaultTableModel();
+        JTable transaction_Table = new JTable(transaction_TableModel);
+        JScrollPane transaction_TableScroll = new JScrollPane(transaction_Table);
+        transaction_TableScroll.setBounds(5, 40, 800, 290);
+        rightPanel.add(transaction_TableScroll);
+
+        transaction_TableModel.addColumn("TRANSACTION ID");
+        transaction_TableModel.addColumn("TOTAL AMOUNT");
+        transaction_TableModel.addColumn("AMOUNT PAID");
+        transaction_TableModel.addColumn("STATUS");
+
+        // Add "ENTER ID" JLabel
+        JLabel enterIdLabel = new JLabel("ENTER ID");
+        enterIdLabel.setBounds(105, 340, 250, 35);
+        rightPanel.add(enterIdLabel);
+
+        // Add JTextField
+        JTextField enterIdTextField = new JTextField();
+        enterIdTextField.setBounds(105, 380, 250, 30);
+        rightPanel.add(enterIdTextField);
+
+        // Add "Enter" JButton
+        JButton enterButton = new JButton("Enter");
+        enterButton.setBounds(105, 420, 120, 30);
+        rightPanel.add(enterButton);
+
+        // Add "more2" JLabel
+        JLabel itemNameLabel = new JLabel("TYPE HERE");
+        itemNameLabel.setBounds(405, 340, 250, 35);
+        rightPanel.add(itemNameLabel);
+
+        // Add "more3" JLabel
+        JLabel itemLengthLabel = new JLabel("LENGTH HERE");
+        itemLengthLabel.setBounds(405, 380, 250, 35);
+        rightPanel.add(itemLengthLabel);
+
+        // Add "more1" JLabel
+        JLabel itemPriceLabel = new JLabel("PRICE HERE");
+        itemPriceLabel.setBounds(405, 420, 250, 35);
+        rightPanel.add(itemPriceLabel);
+
+        
+
+
+
     } // end of New Order
 
     /**
